df9a29af0264cc1dddb5fda6d7cfbe04cd54f193
Beatriz Penteado Correia <beatrizpenteadocorreia@gmail.com>
2024-02-28 22:32:01 -0300

    commit 06

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-<<<<<<< HEAD
- function calcDobro($num1){
-    $dobro = $num * 2;
-=======
  function calcDobro($numero){
     $dobro = $numero * 2;
->>>>>>> dev
     return $dobro;
  }
     function calcTriplo($num1){
         $triplo = $num1 * 3;
         return $triplo;
     }
-
+    function calcTriplo($num1){
+        $triplo = $num1 * 3;
+        return $triplo;
+    }
 
 
 
